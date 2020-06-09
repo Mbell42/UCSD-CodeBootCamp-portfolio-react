21b5ccb738f4e7d3c0bf8002f3c7a02323d2117e
Matthew Bell <matthewbell1030@gmail.com>
2020-06-09 10:43:19 -0700

    heroku build keeps failing, attempting to fix

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
-import NavWrap from "./src/components/navwrap/";
-import NavBar from "./src/components/navbar/";
+import NavWrap from "./src/components/Navwrap/";
+import NavBar from "./src/components/Navbar/";
 
 
 function App() {
