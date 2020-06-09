6f69d3a0b1a85fbb5b40ce3a9cb604918ab7481c
Matthew Bell <matthewbell1030@gmail.com>
2020-06-09 10:31:09 -0700

    attempting to correct imports in App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
-import NavWrap from "./src/components/NavWrap";
-import NavBar from "./src/components/NavBar";
+import NavWrap from "./src/components/NavWrap/index.js";
+import NavBar from "./src/components/NavBar/index.js";
 
 
 function App() {
