65d1e2b4bab1dd3394a13b91e270f342b2e9fbcd
Matthew Bell <matthewbell1030@gmail.com>
2020-06-09 11:04:55 -0700

    committing possible fix for heroku builds

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
-import NavWrap from "./src/components/Navwrap/";
-import NavBar from "./src/components/Navbar/";
+import NavWrap from "./components/NavWrap";
+import NavBar from "./components/NavBar";
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <Router>
             <NavBar />
             <NavWrap>
-              <Route path="/" component={Home} />
+              <Route path="/" component={About} />
               <Route exact path="/about" component={About} />
               <Route exact path="/contact" component={Contact} />
               <Route exact path="/gallery" component={Gallery} />
